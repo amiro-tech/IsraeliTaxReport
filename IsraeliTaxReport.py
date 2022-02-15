ade3ca3eff28870205b4daef6232cf9c54c790a5
Amir Haluts <89452235+amiro-tech@users.noreply.github.com>
2022-02-15 20:04:55 +0200

    Update IsraeliTaxReport.py

--- IsraeliTaxReport.py
+++ IsraeliTaxReport.py
@@ -11,16 +11,20 @@ from currency_converter import CurrencyConverter, ECB_URL  # https://pypi.org/pr
 # noinspection SpellCheckingInspection
 class IsraeliTaxReport:
     """
-    This class is intended to assist with filling out an Israeli annual tax report
-    for Israelis who own stocks in a US brokerage account from which taxes
-    are not automatically paid upon sells of stocks and therefore need to be reported.
-    Currently, it can only handle a report for Interactive Broker (IB).
+    This class is intended to assist with filling out an Israeli annual tax report for Israelis 
+    who own stocks in a US brokerage account from which taxes are not automatically paid upon sells of stocks 
+    and therefore need to be reported. Currently, it can only handle a report from Interactive Brokers data, 
+    but the class can quite easily be edited to handle other formats of raw data by editting its 'keywords' attribute.
 
-    The main method, 'form1325', outputs the lines of form '1325', which is the main form used
-    to report taxable trades and is not trivial to generate directly from the broker report.
-
-    The class has other useful attributes for the tax report, such as 'ustax_paid_df',
+    The main method, 'form1325', outputs the lines of form '1325', which is the main form used to report taxable trades 
+    (and is not trivial to generate directly from the broker report). 
+    
+    The class has other useful attributes for the tax report, such as 'ustax_paid_df', 
     which contain the taxes paid in the us (usually due to dividends).
+
+    Most of the attributes are Pandas dataframes. This class is not optimized for speed 
+    but for ease of reading and submitting tax report data (typically done once or twice a year).
+
     See the README.md page for more details and how to provide a proper input .csv file.
     """
 
