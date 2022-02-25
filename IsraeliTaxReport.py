dff6db27fbfa3e39dae9b7f518729079ea9099ff
Amir Haluts <89452235+amiro-tech@users.noreply.github.com>
2022-02-25 14:27:16 +0200

    Update IsraeliTaxReport.py

--- IsraeliTaxReport.py
+++ IsraeliTaxReport.py
@@ -127,7 +127,116 @@ class IsraeliTaxReport:
                 elif match_logic & exclude_logic:
                     data.append(row)
         return data
+    
+    def partial_sell(self, quant_buy: int, quant_sell: int, tot_quant_buys: int):
+        """
+        Determines if there was a partial sell of the buy
+        
+            Parameters:
+                quant_buy (int): quantity (number of stocks) of current buy
+                quant_sell (int): number of stocks sold in 'sell'
+                tot_quant_buys (int): number of stocks accounted for
 
+            Returns:
+                True if the sale is partial, False otherwise
+        """
+        if quant_buy > quant_sell - tot_quant_buys:
+            quant_buy = quant_sell - tot_quant_buys
+            return True
+        else:
+            return False
+        
+    def apply_fifo(self, row_count, symbol: str, buys, sells, form_dict: dict):
+        for sell in sells.iterrows():
+            # initialize number of stocks accounted for
+            tot_quant_buys = 0
+            # number of stocks sold in 'sell'
+            quant_sell = abs(int(sell[1][self.keywords['quantity']]))
+            while (tot_quant_buys < quant_sell) & (buys.shape[0] > 0):
+                # row number in output form
+                row_count += 1
+                # currnet processed buy
+                buy = buys.head(1)
+                # quantity (number of stocks) of current buy
+                quant_buy = int(buy[self.keywords['quantity']])
+                # in case of partial sell of the buy
+                if quant_buy > quant_sell - tot_quant_buys:
+                    quant_buy = quant_sell - tot_quant_buys
+                    partial = True
+                else:
+                    partial = False
+                # generate row    
+                row = [row_count, symbol].extend(self.calculate_row1325(buy, sell, quant_buy, quant_sell))  
+                # add row to the form dictionary
+                for key, val in zip(form_dict.keys(), row):
+                    form_dict[key].append(val)
+                # update or remove buy (already accounted for)
+                if partial:
+                    buys.iloc[0, :][self.keywords['quantity']] -= quant_buy
+                else:
+                    buys = buys.iloc[1:, :]
+                # update number of stocks accounted for
+                tot_quant_buys += quant_buy
+                
+        return row count, form_dict         
+        
+    def calculate_row1325(self, buy, sell, quant_buy: int, quant_sell: int, round=True):
+        """
+        Calculates a row of the Israeli tax form '1325'
+        
+            Parameters:
+                buy (dataframe): currnet processed buy
+                sell (dataframe): sell of specific stock
+                quant_buy (int): quantity (number of stocks) of current buy
+                quant_sell (int): number of stocks sold in 'sell'
+
+            Returns:
+                A list correponding to the calculated row values of form '1325'
+        """
+        # pre-market hours? (empty in normal trading)
+        pre_hrs = ''
+        # sell total price for batch in USD
+        sold_for_usd = (quant_buy / quant_sell) * abs(float(sell[1][self.keywords['basis']]))
+        # date this batch was bought
+        buy_date = buy[self.keywords['date']].values[0]
+        buy_date = buy_date.split(',')[0]  # keep only date (with IB format)
+        # date this batch was sold
+        sell_date = sell[1][self.keywords['date']].values[0]
+        sell_date = sell_date.split(',')[0]  # keep only date (with IB format)
+        # conversion rate (1 USD in ILS) when bought
+        rate_buy = self.converter.convert(1, self.forex, self.base_currency,
+                                          datetime.strptime(buy_date, '%Y-%m-%d'))
+        # conversion rate (1 USD in ILS) when sold
+        rate_sell = self.converter.convert(1, self.forex, self.base_currency,
+                                           datetime.strptime(sell_date, '%Y-%m-%d'))
+        # cost in USD of batch
+        cost_usd = (min(quant_buy, quant_sell) / quant_buy) * float(buy[self.keywords['basis']])
+        # cost in ILS of batch
+        cost_ils = rate_buy * cost_usd
+        # relative change in conversion rate from sell to buy
+        rate_change = rate_sell / rate_buy
+        # cost in ILS adjusted for relative change in USD to ILS
+        adj_cost_ils = cost_ils * rate_change
+        # sell total price for batch in ILS
+        sold_for_ils = sold_for_usd * rate_sell
+        # if sell price in USD is greater than cost in USD, calculate 'real return'
+        if sold_for_usd > cost_usd:
+            real_return = max(min(sold_for_ils - adj_cost_ils, sold_for_ils - cost_ils), 0)
+            real_loss = ''
+        # if sell price in USD is smaller than cost in USD, calculate 'real loss'
+        else:
+            real_return = ''
+            real_loss = min(max(sold_for_ils - adj_cost_ils, sold_for_ils - cost_ils), 0)
+        # row to be added to form '1325'
+        row = [pre_hrs, quant_buy, sold_for_usd, buy_date, cost_usd, cost_ils,
+               rate_change, adj_cost_ils, sell_date, sold_for_ils, real_return, real_loss]
+        # round number entries in row
+        if round:
+            for i in range(len(row)):
+                if type(row[i]) == float or type(row[i]) == int:
+                    row[i] = round(row[i], 3)
+        return row
+        
     def form1325(self, year: str, save_as_csv=False):
         """
         Returns a Pandas dataframe corresponding to the Israeli tax form '1325'
@@ -154,88 +263,9 @@ class IsraeliTaxReport:
             buys = all_buys.loc[all_buys[self.keywords['symbol']] == symbol]
             # sells of specific stock 'symbol'
             sells = all_sells.loc[all_sells[self.keywords['symbol']] == symbol]
-            for sell in sells.iterrows():
-                # initialize number of stocks accounted for
-                quant_buys = 0
-                # number of stocks sold in 'sell'
-                quant_sell = abs(int(sell[1][self.keywords['quantity']]))
-                while (quant_buys < quant_sell) & (buys.shape[0] > 0):
-                    # row number in output form
-                    row_count += 1
-                    # currnet processed buy
-                    buy = buys.head(1)
-                    # quantity (number of stocks) of current buy
-                    quant = int(buy[self.keywords['quantity']])
-                    # in case of partial sell of the buy
-                    if quant > quant_sell - quant_buys:
-                        quant = quant_sell - quant_buys
-                        partial = True
-                    else:
-                        partial = False
-                    # pre-market hours? (empty in normal trading)
-                    pre_hrs = ''
-                    # sell total price for batch in USD
-                    sold_for_usd = (quant / quant_sell) * abs(float(sells[self.keywords['basis']]))
-                    # date this batch was bought
-                    buy_date = buy[self.keywords['date']].values[0]
-                    buy_date = buy_date.split(',')[0]  # keep only date (with IB format)
-                    # date this batch was sold
-                    sell_date = sells[self.keywords['date']].values[0]
-                    sell_date = sell_date.split(',')[0]  # keep only date (with IB format)
-                    # conversion rate (1 USD in ILS) when bought
-                    rate_buy = self.converter.convert(1, self.forex, self.base_currency,
-                                                      datetime.strptime(buy_date, '%Y-%m-%d'))
-                    # conversion rate (1 USD in ILS) when sold
-                    rate_sell = self.converter.convert(1, self.forex, self.base_currency,
-                                                       datetime.strptime(sell_date, '%Y-%m-%d'))
-                    # cost in USD of batch
-                    cost_usd = (min(quant, quant_sell) / quant) * float(buy[self.keywords['basis']])
-                    # cost in ILS of batch
-                    cost_ils = rate_buy * cost_usd
-                    # relative change in conversion rate from sell to buy
-                    rate_change = rate_sell / rate_buy
-                    # cost in ILS adjusted for relative change in USD to ILS
-                    adj_cost_ils = cost_ils * rate_change
-                    # sell total price for batch in ILS
-                    sold_for_ils = sold_for_usd * rate_sell
-                    # if sell price in USD is greater than cost in USD, calculate 'real return'
-                    if sold_for_usd > cost_usd:
-                        real_return = max(min(sold_for_ils - adj_cost_ils, sold_for_ils - cost_ils), 0)
-                        real_loss = ''
-                    # if sell price in USD is smaller than cost in USD, calculate 'real loss'
-                    else:
-                        real_return = ''
-                        real_loss = min(max(sold_for_ils - adj_cost_ils, sold_for_ils - cost_ils), 0)
-                    # row to be added to form '1325'
-                    row = [row_count,
-                           symbol,
-                           pre_hrs,
-                           quant,
-                           sold_for_usd,
-                           buy_date,
-                           cost_usd,
-                           cost_ils,
-                           rate_change,
-                           adj_cost_ils,
-                           sell_date,
-                           sold_for_ils,
-                           real_return,
-                           real_loss]
-                    # round number entries in row
-                    for i in range(len(row)):
-                        if type(row[i]) == float or type(row[i]) == int:
-                            row[i] = round(row[i], 3)
-                    # add row to the form dictionary
-                    for key, val in zip(form_dict.keys(), row):
-                        form_dict[key].append(val)
-                    # update or remove buy (already accounted for)
-                    if partial:
-                        buys.iloc[0, :][self.keywords['quantity']] -= quant
-                    else:
-                        buys = buys.iloc[1:, :]
-                    # update number of stocks accounted for
-                    quant_buys += quant
-
+            
+            row_count, form_dict = self.apply_fifo(row_count, symbol, buys, sells, form_dict)
+                      
         form1325_df = pd.DataFrame(form_dict)
         # keep only sells in the tax year 'year'
         form1325_df = form1325_df.loc[(form1325_df['תאריך המכירה'] >= year + '-01-01') &
